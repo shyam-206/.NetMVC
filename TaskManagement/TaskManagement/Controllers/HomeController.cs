71941b02c22e2d26b4e0198b43fd985e963969ad
shyam-206 <shyamdhokiya15@gmail.com>
2024-05-03 15:38:43 +0530

    update

--- TaskManagement/TaskManagement/Controllers/HomeController.cs
+++ TaskManagement/TaskManagement/Controllers/HomeController.cs
@@ -13,18 +13,11 @@ namespace TaskManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
+
+
+        public ActionResult Register()
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
